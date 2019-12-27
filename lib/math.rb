9138b2538d519324321461cbf60d73fe3e633ad3
Eddie Hernandez <mchh.004@gmail.com>
2019-12-27 20:30:24 +0000

    Done.

--- lib/math.rb
+++ lib/math.rb
@@ -1,17 +1,20 @@
 def addition(num1, num2)
-
+  Math.addition(num1,num2)
 end
 
 def subtraction(num1, num2)
-
+ difference = num1 - num2
+ return difference
 end
 
 def division(num1, num2)
-
+  quotient = num1 / num2
+  return quotient
 end
 
 def multiplication(num1, num2)
-
+  product = num1 * num2
+  return product
 end
 
 def modulo(num1, num2)
